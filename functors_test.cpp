20a6093ae0235ce8742b51eb0e200c28e1cfd6cc
indro8307 <indra_neel83@yahoo.co.in>
2019-05-13 10:19:19 +0530

    Create functors_test.cpp

---
+++ functors_test.cpp
@@ -0,0 +1,38 @@
+/*
+ * functors_test.cpp
+ *
+ *  Description: Demonstrating the usage of functors.
+ *      
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Adder
+{
+	int operator()(int &sum, int &element){
+		return sum+element;
+	}
+};
+
+int accumulate_impl(vector<int>::iterator begin, vector<int>::iterator end, Adder adder) {
+	int sum = 0;
+	for(vector<int>::iterator it = begin;it!=end;it++) {
+		sum = adder(sum,*it);
+	}
+	return sum;
+}
+
+int main()
+{
+	vector<int> numbers = {1,2,3,4,5};   // It is possible to initialize vectors this way in C++ 11
+	Adder adder;
+	//vector<int>::iterator begin = numbers.begin()
+	int sum = accumulate_impl(numbers.begin(),numbers.end(),adder);
+	cout<<sum;
+	return 0;
+}
+
+

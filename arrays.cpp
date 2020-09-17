53ce5ddb348a8432f732f2dd18d9a73f881b1b3d
indranid <indranid@N-20HEPF0W6553.nsn-intra.net>
2020-09-17 14:13:06 +0530

    Adding arrays.cpp to demonstrate std::array container

---
+++ arrays.cpp
@@ -0,0 +1,87 @@
+
+/******************************************************************************
+
+arrays.cpp
+Description: Demonstrate usage of std::array container in C++
+std::array have the following properties:
+1. stored in stack unlike vectors. Hence quicker access
+2. Aware of its own size unlike C style arrays
+3. Doesn't decompose to pointers upon passing to a function (unlike C style array)
+4. Inheritance friendly unlike C style arrays. Refer below link:
+      https://isocpp.org/wiki/faq/containers#arrays-are-evil
+
+Following std::array methods are demonstrated
+    at() - accesses an element with bound check.
+    data() - returns actual pointer to the array elements i.e. &arr[0]
+
+*******************************************************************************/
+
+#if 1
+#include <iostream>
+#include <array>
+
+
+class Employee
+{
+	int m_id = 0;
+	std::string m_name = "";
+public:
+	Employee(int id,std::string name) : m_id{id}, m_name{name} {}
+	Employee() = default;
+	~Employee() {}
+	friend std::ostream& operator<< (std::ostream& os, const Employee& e);
+	void operator() (int id, std::string name)
+	{
+		m_id = id; m_name = name;
+	}
+
+};
+
+std::ostream& operator<< (std::ostream& os, const Employee& e)
+{
+	os<<"id = " <<e.m_id << " name = " << e.m_name ;
+	return os;
+}
+
+void func(Employee arr[], int size)
+{
+	for(int i=0;i<size;++i)
+	{
+		std::cout<<arr[i]<<std::endl;
+	}
+}
+
+int main()
+{
+	std::array<Employee,5> emparr {Employee{1,"John"},
+		                            Employee{2,"Dave"},
+									Employee{3,"han"},
+									Employee{4,"lol"},
+									Employee{5,"jami"},
+	                               };
+
+	//// unlike C style arrays, array containers do boundary check,
+	//// but only if you use at() to access elements
+	//// Below code snippet throws out_of_range exception
+	//// as there are no elements at index 6
+	try{
+	std::cout<<emparr.at(6)<<std::endl;
+	} catch(std::out_of_range const& e)
+	{
+		std::cout<<e.what()<<std::endl;
+	}
+
+	//// can also be treated as a normal C style array
+	func(emparr.data(), emparr.size());
+
+    //// can also change values using at() because at() returns an lvalue, i.e. a reference
+	emparr.at(0)(6,"Sam");
+	func(emparr.data(), emparr.size());
+
+	//// or u can simply use the C style but this DOES NOT do boundary check !!!!
+	emparr[2](7,"Jenny");
+	std::cout<<"printing in C style "<<emparr[2]<<std::endl;
+	return 0;
+}
+
+#endif
